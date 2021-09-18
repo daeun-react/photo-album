d3d096b989cfc146407aa3fd0a26603d73b09d07
daeun-react <smile_daeun@naver.com>
2021-09-18 18:25:56 +0900

    feat : 모달창에 보일 PostInfo 컴포넌트 생성

---
+++ src/components/post/PostInfo.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { history } from "redux/configureStore";
+import PostHeader from "components/post/PostHeader";
+import { StyledButton } from "components/common/Styled";
+
+function PostInfo({ image_url, user_info, insert_dt, is_me, id, type }) {
+  return (
+    <Wrapper>
+      <PostImage image_url={image_url} />
+      <PostInfoWrapper>
+        <PostHeader
+          type="modal"
+          user_info={user_info}
+          insert_dt={insert_dt}
+          is_me={is_me}
+          id={id}
+        />
+        <CommentWrapper></CommentWrapper>
+
+        {type === "modal" && (
+          <StyledButton
+            width="90%"
+            padding="12px"
+            margin="12px"
+            onClick={() => history.push(`/post/${id}`)}>
+            게시글로 이동
+          </StyledButton>
+        )}
+      </PostInfoWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+`;
+
+const PostImage = styled.div`
+  background: url("${({ image_url }) => image_url}");
+  background-size: cover;
+`;
+
+const PostInfoWrapper = styled.div`
+  ${({ theme }) => theme.flexSet("flex-start", "center", "column")};
+  background-color: #fff;
+`;
+
+const CommentWrapper = styled.div`
+  width: 100%;
+  height: calc(50vh - 100px);
+`;
+export default PostInfo;
