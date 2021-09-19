fa281b0c5d2dea420f87dcfae997d999944be932
daeun-react <smile_daeun@naver.com>
2021-09-19 13:04:36 +0900

    feat : redux-persist 메인 index.js 에 적용

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import ReduxThunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer, { history } from "redux/configureStore";
@@ -15,13 +17,16 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(ReduxThunk.withExtraArgument({ history })))
 );
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={{ ...Mixin }}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={{ ...Mixin }}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,15 @@ import user from "redux/modules/user";
 import post from "redux/modules/post";
 import image from "redux/modules/image";
 import comment from "redux/modules/comment";
+import drawer from "redux/modules/drawer";
+import storage from "redux-persist/lib/storage/session";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user", "post", "image", "comment", "drawer", "router"],
+};
 
 export const history = createBrowserHistory();
 const rootReducer = combineReducers({
@@ -12,7 +21,8 @@ const rootReducer = combineReducers({
   post,
   image,
   comment,
+  drawer,
   router: connectRouter(history),
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
