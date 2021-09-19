3f58605353305bc2f11f235a4ffd300446064ee1
daeun-react <smile_daeun@naver.com>
2021-09-20 00:59:27 +0900

    feat : 게시글로 이동 시 모달창 닫기 추가

--- src/components/post/PostInfo.js
+++ src/components/post/PostInfo.js
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
 import { history } from "redux/configureStore";
+import { actionCreators as modalActions } from "redux/modules/modal";
+import { ROUTES } from "utils/constants";
 import PostHeader from "components/post/PostHeader";
 import { StyledButton } from "components/common/Styled";
 import CommentList from "components/comment/CommentList";
 
 function PostInfo({ image_url, user_info, insert_dt, is_me, id, type }) {
+  const { POST } = ROUTES;
+  const dispatch = useDispatch();
+
+  const MoveToPostPage = () => {
+    history.push(`${POST}/${id}`);
+    dispatch(modalActions.closeModal(false));
+  };
+
   return (
     <Wrapper>
       <PostImage image_url={image_url} />
@@ -22,11 +33,7 @@ function PostInfo({ image_url, user_info, insert_dt, is_me, id, type }) {
         </CommentWrapper>
 
         {type === "modal" && (
-          <StyledButton
-            width="90%"
-            padding="12px"
-            margin="12px"
-            onClick={() => history.push(`/post/${id}`)}>
+          <StyledButton width="90%" padding="12px" margin="12px" onClick={MoveToPostPage}>
             게시글로 이동
           </StyledButton>
         )}
